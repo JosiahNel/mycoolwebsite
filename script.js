303eb02fa89721f72879418d643ebb4fb6ddd9d3
JosiahNel <95706200+JosiahNel@users.noreply.github.com>
2023-11-10 15:18:20 -0600

    Danger!!

---
+++ script.js
@@ -0,0 +1,16 @@
+for (let i = 100; i > 0; i++) {
+  console.log('Good Luck!');
+  console.log('Surviving this attack :)');
+  console.log('Good Luck!');
+  console.log('Surviving this attack :)');
+  console.log('Good Luck!');
+  console.log('Surviving this attack :)');
+  console.log('Good Luck!');
+  console.log('Surviving this attack :)');
+  console.log('Good Luck!');
+  console.log('Surviving this attack :)');
+  console.log('Good Luck!');
+  console.log('Surviving this attack :)');
+  console.log('Good Luck!');
+  console.log('Surviving this attack :)');
+}
